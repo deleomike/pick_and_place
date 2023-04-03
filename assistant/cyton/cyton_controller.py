1a0765a986f8d57103ef0be8940445cca0a4bc4f
deleomike <michaeldeleo31@yahoo.com>
2023-04-03 18:11:26 -0400

    Basic Controller connection

--- assistant/cyton/cyton_controller.py
+++ assistant/cyton/cyton_controller.py
@@ -1,3 +1,5 @@
+import socket
+
 from assistant.cyton.cyton import CytonGamma300
 from spatialmath import SE3
 from assistant.items.BaseItem import BaseItem
@@ -20,6 +22,10 @@ class CytonController:
 
         self.connect = connect
 
+        self.sock: socket.socket = None
+        self.udp_ip = None
+        self.udp_port = None
+
         if self.connect:
             self.establish_connection()
 
@@ -27,14 +33,18 @@ class CytonController:
 
         self.set_pose(self.pose)
 
-    def establish_connection(self) -> bool:
+    def establish_connection(self, udp_ip: str = "127.0.0.1", udp_port: int = 5005) -> bool:
         """
         Establishes a connection to the robot
         :return: True/False whether the connection was successful
         """
-        print(f"Establishing Connection to {self.robot.name}")
+        print(f"Establishing Connection to {self.robot.name} at {udp_ip}:{udp_port}")
 
-        # TODO
+        self.udp_ip = udp_ip
+        self.udp_port = udp_port
+
+        self.sock = socket.socket(socket.AF_INET,  # Internet
+                                  socket.SOCK_DGRAM)  # UDP
 
     def set_pose(self, q: SE3):
         """
@@ -43,9 +53,27 @@ class CytonController:
         :return:
         """
         # TODO
-        pass
+
+        if self.connect:
+            try:
+                self.sock.send(q)
+            except Exception as e:
+                # recreate the socket and reconnect
+                print(f"Error connecting to {self.udp_ip}:{self.udp_port}. Reconnecting")
+                self.establish_connection(self.udp_ip, self.udp_port)
+
+                self.sock.send(q)
+        # TODO: fake printouts
+
+    def go_home(self):
+        self.set_pose(self.robot.qz)
 
     def grab_object(self, item: BaseItem):
         robot_object_pose = self.robot.ikine_LM(item.pose, q0=self.pose)
 
         traj = jtraj(self.pose, robot_object_pose, 100)
+
+
+if __name__ == "__main__":
+
+    controller = CytonController(connect=True)
