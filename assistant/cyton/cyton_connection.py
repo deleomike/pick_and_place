78189c4548ee529c089855803c64fe22f8fd0ae4
Michael DeLeo <michaeldeleo31@yahoo.com>
2023-04-19 19:59:46 -0400

    Send and wait

--- assistant/cyton/cyton_connection.py
+++ assistant/cyton/cyton_connection.py
@@ -1,22 +1,25 @@
 
 import socket
+import time
+
 import numpy as np
 
 from typing import List
 from assistant.networking import UDPServer, UDPClient
+from assistant.cyton.cyton_dummy import CytonDummyServer
 
 
 class CytonConnection:
 
-    def __init__(self, ip: str = "127.0.0.1", send_port: int = 8888, recv_port: int = 8889):
+    def __init__(self, ip: str = "127.0.0.1", send_port: int = 8888, recv_port: int = 8889, verbose: bool = False):
         self.ip = ip
         self.recv_port = recv_port
         self.send_port = send_port
 
         self.client = UDPClient(ip=self.ip, port=self.send_port)
-        self.listener = UDPServer(ip=self.ip, listen_port=self.recv_port)
+        self.listener = CytonDummyServer(listen_port=self.recv_port, round_to=4, verbose=verbose )
 
-        # self.listener.start()
+        self.listener.start()
 
     def __del__(self):
         self.disconnect()
@@ -25,7 +28,20 @@ class CytonConnection:
         self.client.disconnect()
         self.listener.disconnect()
 
-    def send_angles(self, q: List[float]):
+    def is_cyton_at_q(self, q: List[float]) -> bool:
+        """
+        Checks if the cyton is at the set of angles
+        """
+        ground_truth_angles = self.listener.current_angles
+
+        q_np = np.array(q)
+
+        return (q_np == ground_truth_angles).all()
+
+    def send_angles(self, q: List[float], wait: bool = True):
+        """
+        Sends angles to cyton. Can also wait for cyton reach these angles
+        """
 
         data = np.array(q, dtype=np.double)
         data = data.view(np.uint8)
@@ -36,13 +52,23 @@ class CytonConnection:
             print(f"Could not send data to {self.ip}:{self.send_port} {e}")
             raise e
 
+        while wait:
+            if self.is_cyton_at_q(q=q):
+                break
+            else:
+                time.sleep(0.1)
+
 
 if __name__ == "__main__":
 
     connection = CytonConnection(send_port=8888, recv_port=8889)
 
-    # connection.send_angles([0, 0.7, 0, 0.7, 0, 0.7, 0, 0.01])
+    connection.send_angles([0, 0.7, 0, 0.7, 0, 0.7, 0, 0.01])
 
     # connection.send_angles([0, 0, 0, 0.7, 0, 0.7, 0, 0.01])
 
     connection.send_angles([0, 0, 0, 0.4, 0, 0, 0, 0.0])
+
+    print("Done")
+
+    connection.disconnect()
